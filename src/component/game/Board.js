0e29d437a6bc1e2e480e87ab8658ff973c1ace39
Ryan <ryanfrost2015@gmail.com>
2020-12-03 02:23:05 +0000

    player can move piece anywhere & kill enemy piece

--- src/component/game/Board.js
+++ src/component/game/Board.js
@@ -79,11 +79,9 @@ function Board({db, authInfo, position}) {
                 let styleVal = {
                     gridRow: `${item.piece.rowPosition}`,
                     gridColumn:`${item.piece.columnPosition}`,
-                    backgroundColor: item.piece.white ? "white" : "red",
-                    // height: `inherit`,
                 }
                 let gridItem = (
-                    <div data-piece={item.piece.name} data-color={item.piece.white ? "white" : "red"} style={styleVal} key={Math.random()}>
+                    <div id={`${item.piece.white ? "white" : "black"}${item.piece.name}`} className={item.piece.white ? "white" : "black"} data-color={item.piece.white ? "white" : "black"} style={styleVal} key={Math.random()}>
                     {item.piece.name}</div>
                     ); //data-name={item.piece.name}
                 arrayOfJSXPieces.push(gridItem);
@@ -103,7 +101,6 @@ function Board({db, authInfo, position}) {
             let styleVal = {
                 gridRow: `${row}`,
                 gridColumn:`${col}`,
-                // height: `40px`,
             }
             let square = (
             <div id={index} data-square style={styleVal} onClick={onClickHandler} key={Math.random()}>
@@ -123,15 +120,6 @@ function Board({db, authInfo, position}) {
 
     //2.5 CODE TO EXECUTE WHEN THINGS"RE RENDERED ON BOARD
     useEffect(() => {
-        //doesn't fire on initial execution
-        if (initialExecutionGridItemsUseEffect.current) {
-            initialExecutionGridItemsUseEffect.current = false;
-            return;
-        }
-        console.log("waited for gridItems state to change", gridItems);
-        //set square's CSS height to be explicit value so piece will inherit this value and become same height as square
-        let board = document.querySelector('.board-grid-container');
-
     }, [gridItems])
 
     //3.ONCLICKHANDLER
@@ -142,41 +130,84 @@ function Board({db, authInfo, position}) {
         //     console.log("target", e.target);
         //     return;
         // }
-        //ALREADY CLICKED ON PIECE?
-        console.log("clickedOnPiece:", clickedOnPiece.current);
-        if (clickedOnPiece.current) { //clickedOnPiece
-            //IS SAME PIECE?
-            if (e.currentTarget.id === clickedOnPiece.current.id && e.currentTarget.dataset.color === clickedOnPiece.current.color) {
+        console.log("clickedOnPiece:", clickedOnPiece.current); console.log("e.target:", e.target);
+        if (clickedOnPiece.current) { 
+            //HAVE PREVIOUSLY CLICKED ON PIECE
+            console.log("2nd stage", e.target.id, clickedOnPiece.current.id);
+            let originalPiece = window.document.querySelector(`#${clickedOnPiece.current.id}`);
+            if (e.target.id === clickedOnPiece.current.id) {
+                //SAME PIECE: DESELECT
+                const piece = e.target;
+                //unhighlight
+                piece.classList.remove(`highlighted`);
+                //reset state
                 clickedOnPiece.current = false;
-                console.log("piece deselected", clickedOnPiece.current);
+                console.log("piece deselected"); //console.log("current state:", clickedOnPiece.current);
                 return;
-            }
-            console.log("2nd stage");   
+            } 
+            if (e.target.dataset.color === clickedOnPiece.current.color) {
+                //OUR PIECE: INVALID
+                //unhighlight orginal piece
+                originalPiece.classList.remove(`highlighted`);
+                //highlight clicked on piece
+                e.target.classList.add(`highlighted`);
+                //reset state
+                clickedOnPiece.current = {color: e.target.dataset.color, id: e.target.id};
+                console.log("invalid - moving on our piece.", "new state:", clickedOnPiece.current);
+                return;
+            }  
             //IS LEGAL MOVE?
             //is square empty?
             //if not, is opponent there? Is opponent king?
 
-            //find GI clicked on & apply CSS on it so that it moves. 
-            let clickedOnEl = window.document.querySelector(`#${clickedOnPiece.id}`);
-            console.log(e.currentTarget);
-            // let newCoordinates = 
+            //MOVE find GI clicked on & apply CSS on it so that it becomes child of square.
+            console.log("executing", "original piece:", originalPiece);
+            if (e.target.dataset.square) {
+                //secondaryEl IS AN EMPTY SQUARE
+                let emptySquare = e.target;
+                emptySquare.append(originalPiece);
+                console.log("secondaryEl is an empty square:", e.target);
+                unhighlightOriginalPieceResetState();
+                //
+            } else {
+                //secondaryEl IS AN ENEMY PIECE
+                const enemyPiece = e.target;
+                const enemyPieceSquare = e.currentTarget;
+                //kill enemy
+                enemyPiece.classList.add(`fizzle`);
+                setTimeout(() => {
+                    enemyPiece.remove();
+                    //append originalPiece
+                    enemyPieceSquare.append(originalPiece);
+                    unhighlightOriginalPieceResetState();
+                }, 2000);
+            }
             //send this info to db full grid-area property to dB property. 
-            //reset state
-            console.log(clickedOnEl); 
-            // setClickedOnPiece(false);
-            return;
+            function unhighlightOriginalPieceResetState() {
+                //unhighlight original piece
+                originalPiece.classList.remove(`highlighted`);
+                //reset state
+                clickedOnPiece.current = false;
+                console.log(clickedOnPiece.current); 
+            }
+        } else {
+            //HAVEN'T PREVIOUSLY CLICKED PIECE (FRESH)
+            console.log("1st stage"); console.log(e.target.dataset.color, e.target.dataset.square, e.target.id, userColor.current);
+            if (e.target.dataset.square || e.target.dataset.color !== userColor.current) {
+                //FRESHLY CLICKED ON EMPTY SQUARE OR OPPONENT PIECE OR OFF-BOARD
+                console.log("exited");
+                e.preventDefault();
+                return;
+            }
+            //FRESHLY CLICKED ON OWN PIECE
+            let piece = e.target;
+            //REGISTER, IN STATE, INFO ON PIECE CLICKED ON
+            clickedOnPiece.current = {color: piece.dataset.color, id: piece.id};
+            console.log(clickedOnPiece.current);
+            //HIGHLIGHT SQUARE/PIECE
+            // piece.style.setProperty('color', 'pink');
+            piece.classList.add(`highlighted`);
         }
-        console.log("1st stage");
-        //CHECK ITS OUR PIECE
-        // if (e.currentTarget.dataset.color !== userColor.current) {
-        //     e.preventDefault();
-        //     return
-        // }
-        //REGISTER, IN STATE, INFO ON PIECE CLICKED ON
-        // setClickedOnPiece("red");//setClickedOnPiece({color: e.currentTarget.dataset.color, id: e.currentTarget.id});
-        clickedOnPiece.current = {color: e.currentTarget.dataset.color, id: e.currentTarget.id};
-        console.log(clickedOnPiece.current);
-        //HIGHLIGHT SQUARE/PIECE
     }
 
     //4. LEGAL-MOVE LOGIC HELPER FUNCTIONS
