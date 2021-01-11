4bbd41ced2339ea9cdd3fd17cbe197b9c316d65b
Ryan <ryanfrost2015@gmail.com>
2021-01-11 23:31:11 +0000

    Test to see if app doesn't require npm run build ontop of new commit to be relaunched by netlify 2

--- src/component/home/Instructions.js
+++ src/component/home/Instructions.js
@@ -3,7 +3,7 @@ export default function Instructions({ url }) {
         <div className="instructions">
             {/* <p>Instructions</p> */}
             <p>
-                Test to see if app doesn't require npm run build ontop of new commit to be relaunched by netlify.
+                Test to see if app doesn't require npm run build ontop of new commit to be relaunched by netlify FOR REAL
                 Clicking Start New Game will direct you to a random url.
                 Share this url with another player for them to join.
                 {url ? <span><br></br>Url: {window.location.origin}/{url}</span> : null}
