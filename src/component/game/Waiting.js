b88f476d4d435bf3ba234dc934a18a0371253bec
Ryan <ryanfrost2015@gmail.com>
2021-01-14 20:07:42 +0000

    .pawn-promotion-container CSS debug1

--- src/component/game/Waiting.js
+++ src/component/game/Waiting.js
@@ -3,7 +3,7 @@ import Instructions from '../home/Instructions';
 function Waiting(params) {
     return (
         <>
-            <p className="waiting-for-user2-sign-in">Share this url with another player for them to join. You may need to refresh the page after they've joined.</p>
+            <p className="waiting-for-user2-sign-in">Share this url with another player for them to join.</p>
         </> //'after they've joined'?
     )
 }
