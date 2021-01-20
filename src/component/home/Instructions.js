48bea4e8e2073afc7a07fbd370ec04d2dc9bb36a
Ryan <ryanfrost2015@gmail.com>
2021-01-20 01:03:12 +0000

    save before highlight opponent's moved from/to squares

--- src/component/home/Instructions.js
+++ src/component/home/Instructions.js
@@ -3,7 +3,7 @@ export default function Instructions({ url }) {
         <div className="instructions">
             {/* <p>Instructions</p> */}
             <p>
-                save before castling.
+                save before highlight opponent's moved from/to squares.
                 Clicking Start New Game will direct you to a random url.
                 Share this url with another player for them to join.
                 {url ? <span><br></br>Url: {window.location.origin}/{url}</span> : null}
