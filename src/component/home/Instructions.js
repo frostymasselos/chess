57950af5bcf146c510d6f0549f88fc984ae93b52
Ryan <ryanfrost2015@gmail.com>
2021-01-24 23:30:14 +0000

    exp3.6

--- src/component/home/Instructions.js
+++ src/component/home/Instructions.js
@@ -3,7 +3,7 @@ export default function Instructions({ url }) {
         <div className="instructions">
             {/* <p>Instructions</p> */}
             <p>
-                "exp3.5"
+                "exp3.6"
                 Clicking Start New Game will direct you to a random url.
                 Share this url with another player for them to join.
                 {url ? <span><br></br>Url: {window.location.origin}/{url}</span> : null}
