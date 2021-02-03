f7caebf486840467bc96c29637e76cd12b3f31f8
Ryan <ryanfrost2015@gmail.com>
2021-02-03 12:29:11 +0000

    save before merge

--- src/component/home/Instructions.js
+++ src/component/home/Instructions.js
@@ -3,7 +3,7 @@ export default function Instructions({ url }) {
         <div className="instructions">
             <p>
                 Clicking Start New Game will direct you to a random url.
-                Share this url with another player for them to join. Develop branch.
+                Share this url with another player for them to join.
                 {url ? <span><br></br>Url: {window.location.origin}/{url}</span> : null}
             </p>
         </div>
