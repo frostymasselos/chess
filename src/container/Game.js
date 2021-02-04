f7850c756cee8c7d966b09db3d6ac9f53e481439
Ryan <ryanfrost2015@gmail.com>
2021-02-04 18:54:02 +0000

    using userMoveSound for both

--- src/container/Game.js
+++ src/container/Game.js
@@ -374,16 +374,18 @@ function Game({ params }) {
             </div>}
             <img src={soundState ? soundSVG : muteSVG} className={`audio-icon ${soundState ? '' : 'sound-off'}`} onClick={turnSoundOnOrOff} />
             <audio controls muted className="user-move-audio-tag">
-                <source src={userMoveSound} type="audio/mp3"></source>
+                <source src={userMoveSound}></source>
             </audio>
             <audio controls muted className="opp-move-audio-tag">
-                <source src={oppMoveSound} type="audio/mp3"></source>
+                <source src={userMoveSound}></source>
             </audio>
         </>
     )
 }
 
 export default Game
+// oppMoveSound
+// type="audio/mp3"
 
 
 
