c64c5979cfaae3121830aebca2927f9213540d34
Ryan <ryanfrost2015@gmail.com>
2021-02-24 13:10:15 +0000

    first commit

--- src/component/home/Header.js
+++ src/component/home/Header.js
@@ -6,6 +6,9 @@ function Header() {
     return (
         <>
             <div className="header">
+                <a href="https://www.youtube.com/watch?v=h7tQXFXTJ5k&feature=youtu.be" target="_blank">
+                    Demonstration
+                </a>
                 <Link to="/">
                     <img className="piece-image" src={returnPieceEmoji("queen")} />
                     <img className="piece-image" src={returnPieceEmoji("knight")} />
